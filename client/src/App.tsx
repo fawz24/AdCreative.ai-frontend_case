f1fcb35915e1f3d27fb7e50dbc4c2c8348e2a4a0
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2023-10-15 21:42:35 +0300

    using local storage to restore selected categories

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import "./App.css";
 import { getAllCategories, searchCategories } from "./services/category";
 import { SearchIcon } from "./components/SearchIcon";
 import { CategoryEntry } from "./type";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [categories, setCategories] = useState<CategoryEntry[]>([]);
-  const [selectedCategories, setSelectedCategories] = useState<CategoryEntry[]>(
-    []
-  );
+  const [selectedCategories, setSelectedCategories] = useLocalStorage<
+    CategoryEntry[]
+  >("selectedCategories", []);
 
   const selectedCategoryIds = selectedCategories.map(({ id }) => id);
   const unselectedCategories = categories.filter(
@@ -32,7 +33,8 @@ function App() {
     });
   };
 
-  const search = async () => {
+  const search: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
     const result = await searchCategories({
       selectedCategoryIds,
       query: searchQuery,
@@ -51,7 +53,10 @@ function App() {
 
   return (
     <main className="p-10">
-      <div className="border border-gray-2 rounded-md bg-gray-1 p-6 w-max flex flex-col items-start mx-auto gap-3">
+      <form
+        className="border border-gray-2 rounded-md bg-gray-1 p-6 w-max flex flex-col items-start mx-auto gap-3"
+        onSubmit={search}
+      >
         <h2>Kategoriler</h2>
         <label className="w-full flex justify-between items-center p-2 rounded-md text-gray-3 bg-white border border-gray-2 focus-within:outline-2 focus-within:outline-gray-2 focus-within:outline">
           <input
@@ -66,40 +71,75 @@ function App() {
           <SearchIcon className="pointer-events-none" />
         </label>
         <div className="max-h-80 overflow-y-auto">
-          <ul>
-            {selectedCategories
-              .concat(unselectedCategories)
-              .map((categoryEntry) => {
-                const { id, category } = categoryEntry;
-                return (
-                  <li key={id} value={category}>
-                    <label>
-                      <input
-                        type="checkbox"
-                        name="category"
-                        value={id}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            selectCategory(categoryEntry);
-                          } else {
-                            unselectCategory(categoryEntry);
-                          }
-                        }}
-                      />
-                      {category}
-                    </label>
-                  </li>
-                );
-              })}
+          <ul
+            onKeyDown={(event) => {
+              if (event.code === "Enter") {
+                event.preventDefault();
+                return false;
+              }
+            }}
+          >
+            {selectedCategories.map((categoryEntry) => {
+              const { id, category } = categoryEntry;
+              return (
+                <li key={id} value={category}>
+                  <label className="flex gap-4">
+                    <input
+                      type="checkbox"
+                      name="category"
+                      value={id}
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          selectCategory(categoryEntry);
+                        } else {
+                          unselectCategory(categoryEntry);
+                        }
+                      }}
+                      className="peer"
+                      checked={true}
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                      }}
+                    />
+                    <span className="peer-checked:text-blue-2">{category}</span>
+                  </label>
+                </li>
+              );
+            })}
+            {unselectedCategories.map((categoryEntry) => {
+              const { id, category } = categoryEntry;
+              return (
+                <li key={id} value={category}>
+                  <label className="flex gap-4">
+                    <input
+                      type="checkbox"
+                      name="category"
+                      value={id}
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          selectCategory(categoryEntry);
+                        } else {
+                          unselectCategory(categoryEntry);
+                        }
+                      }}
+                      className="peer"
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                      }}
+                    />
+                    <span className="peer-checked:text-blue-2">{category}</span>
+                  </label>
+                </li>
+              );
+            })}
           </ul>
         </div>
-        <button
-          className="bg-blue text-white w-full p-2 rounded-md"
-          onClick={search}
-        >
+        <button className="bg-blue-1 text-white w-full p-2 rounded-md">
           Ara
         </button>
-      </div>
+      </form>
     </main>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: "#264cc5",
+        "blue-1": "#264cc5",
+        "blue-2": "#254cc6",
         "gray-1": "#f7f7f7",
         "gray-2": "#cccbc5",
         "gray-3": "#4f4f4f",
